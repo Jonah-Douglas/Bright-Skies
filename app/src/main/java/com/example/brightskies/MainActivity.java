79a493655daa932df5c698e50417450acd00f0b3
Piper <wolterp@wwu.edu>
2020-02-10 12:55:07 -0800

    added another screen, added silly images

--- app/src/main/java/com/example/brightskies/MainActivity.java
+++ app/src/main/java/com/example/brightskies/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.brightskies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.EditText;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = ".com.example.brightskies.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Called when the user taps the Discover Your Bright Skies button */
+    public void enterApp(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
 }
