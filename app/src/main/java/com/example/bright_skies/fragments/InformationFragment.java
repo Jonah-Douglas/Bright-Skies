c775db9af0c91ea90098f92e0fefb189d1358445
Melody Grappo <grappom@wwu.edu>
2020-02-25 13:12:47 -0800

    app organization changes

--- app/src/main/java/com/example/bright_skies/fragments/InformationFragment.java
+++ app/src/main/java/com/example/bright_skies/fragments/InformationFragment.java
@@ -4,12 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseExpandableListAdapter;
 import android.widget.ExpandableListView;
-import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 
 import com.example.bright_skies.R;
+import com.example.bright_skies.managers.ExpandableListAdapter;
 
 public class InformationFragment extends Fragment {
 
@@ -17,12 +16,7 @@ public class InformationFragment extends Fragment {
     ExpandableListView lv;
     private String[] groups;
     private String[][] children;
-
-
-    public InformationFragment() {
-
-    }
-
+    
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -48,103 +42,9 @@ public class InformationFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         lv = view.findViewById(R.id.expandableListView);
-        lv.setAdapter(new ExpandableListAdapter(groups, children));
+        lv.setAdapter(new ExpandableListAdapter(this, groups, children));
         lv.setGroupIndicator(null);
 
     }
 
-    public class ExpandableListAdapter extends BaseExpandableListAdapter {
-
-        private final LayoutInflater inf;
-        private String[] groups;
-        private String[][] children;
-
-        public ExpandableListAdapter(String[] groups, String[][] children) {
-            this.groups = groups;
-            this.children = children;
-            inf = LayoutInflater.from(getActivity());
-        }
-
-        @Override
-        public int getGroupCount() {
-            return groups.length;
-        }
-
-        @Override
-        public int getChildrenCount(int groupPosition) {
-            return children[groupPosition].length;
-        }
-
-        @Override
-        public Object getGroup(int groupPosition) {
-            return groups[groupPosition];
-        }
-
-        @Override
-        public Object getChild(int groupPosition, int childPosition) {
-            return children[groupPosition][childPosition];
-        }
-
-        @Override
-        public long getGroupId(int groupPosition) {
-            return groupPosition;
-        }
-
-        @Override
-        public long getChildId(int groupPosition, int childPosition) {
-            return childPosition;
-        }
-
-        @Override
-        public boolean hasStableIds() {
-            return true;
-        }
-
-        @Override
-        public View getChildView(int groupPosition, final int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-
-            ViewHolder holder;
-            if (convertView == null) {
-                convertView = inf.inflate(R.layout.information_list_item, parent, false);
-                holder = new ViewHolder();
-
-                holder.text = convertView.findViewById(R.id.lblListItem);
-                convertView.setTag(holder);
-            } else {
-                holder = (ViewHolder) convertView.getTag();
-            }
-
-            holder.text.setText(getChild(groupPosition, childPosition).toString());
-
-            return convertView;
-        }
-
-        @Override
-        public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-            ViewHolder holder;
-
-            if (convertView == null) {
-                convertView = inf.inflate(R.layout.information_list_group, parent, false);
-
-                holder = new ViewHolder();
-                holder.text = convertView.findViewById(R.id.lblListHeader);
-                convertView.setTag(holder);
-            } else {
-                holder = (ViewHolder) convertView.getTag();
-            }
-
-            holder.text.setText(getGroup(groupPosition).toString());
-
-            return convertView;
-        }
-
-        @Override
-        public boolean isChildSelectable(int groupPosition, int childPosition) {
-            return true;
-        }
-
-        private class ViewHolder {
-            TextView text;
-        }
-    }
 }
