f7ea04988c2ef0f758b7ff80aac19d055d3e5e0a
Zack <zackrdoll@gmail.com>
2020-02-29 13:10:04 -0800

    a mapview is appearing on the map page nowgit status!

--- app/src/main/java/com/example/bright_skies/fragments/MapFragment.java
+++ app/src/main/java/com/example/bright_skies/fragments/MapFragment.java
@@ -1,6 +1,7 @@
 package com.example.bright_skies.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,20 +11,58 @@ import androidx.fragment.app.Fragment;
 
 import com.example.bright_skies.R;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+//import com.google.android.gms.maps.MapFragment;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
+    private MapView mMapView;
+    private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_map, container, false);
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapView);
-        mapFragment.getMapAsync(this);
+//        MapFragment mapFragment = (MapFragment) getChildFragmentManager().findFragmentById(R.id.mapView);
+//        mapFragment.getMapAsync(this);
+        Bundle mapViewBundle = null;
+
+        mMapView = (MapView) root.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+        mMapView.getMapAsync(this);
         return root;
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
 
+        Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        mMapView.onSaveInstanceState(mapViewBundle);
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(0,0)).title("Marker"));
+    }
+
+    @Override
+    public void onPause() {
+        mMapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        Log.d("TEST", "entered onResume");
+        super.onResume();
+        mMapView.onResume();
+        Log.d("TEST", "leaving onResume");
+    }
+
 }
