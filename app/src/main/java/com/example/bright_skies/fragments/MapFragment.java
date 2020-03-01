90c5ba11133fbc2fb12824252f3615a0e5ade4bc
Zack <zackrdoll@gmail.com>
2020-02-29 17:52:20 -0800

    updated camera postion of mapview

--- app/src/main/java/com/example/bright_skies/fragments/MapFragment.java
+++ app/src/main/java/com/example/bright_skies/fragments/MapFragment.java
@@ -2,6 +2,7 @@ package com.example.bright_skies.fragments;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,15 +16,26 @@ import androidx.fragment.app.Fragment;
 
 import com.example.bright_skies.R;
 import com.example.bright_skies.activities.MainActivity;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.Objects;
+
+import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
 //import com.google.android.gms.maps.MapFragment;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
     private MapView mMapView;
+    private FusedLocationProviderClient mFusedLocationClient;
     private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -35,6 +47,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 //            != PackageManager.PERMISSION_GRANTED) {
 //            ActivityCompat.requestPermissions(MainActivity, Manifest.permission.ACCESS_FINE_LOCATIONS);
 //        }
+
         mMapView = (MapView) root.findViewById(R.id.mapView);
         mMapView.onCreate(savedInstanceState);
         mMapView.getMapAsync(this);
@@ -55,9 +68,33 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(0,0)).title("Marker"));
+    public void onMapReady(final GoogleMap googleMap) {
+        mFusedLocationClient = getFusedLocationProviderClient(Objects.requireNonNull(getContext()));
+        mFusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                if (task.isSuccessful()) {
+                    Log.d("TEST","entered successful");
+                    Location location = task.getResult();
+                    double lat = location.getLatitude();
+                    double lon = location.getLongitude();
+                    LatLng userLatLng = new LatLng(lat, lon);
+                    Log.d("TEST", String.valueOf(lat));
+                    Log.d("TEST", String.valueOf(lon));
+                    CameraPosition cam_pos = new CameraPosition.Builder()
+                            .target(userLatLng)
+                            .zoom(18)
+                            .tilt(45)
+                            .build();
+                    googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cam_pos));
+//                    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLatLng, 20));
+                    googleMap.addMarker(new MarkerOptions().position(userLatLng).title("Your Location"));
+                }
+            }
+        });
+//        googleMap.addMarker(new MarkerOptions().position(new LatLng(0,0)).title("Marker"));
         googleMap.setMyLocationEnabled(true);
+
     }
 
     @Override
