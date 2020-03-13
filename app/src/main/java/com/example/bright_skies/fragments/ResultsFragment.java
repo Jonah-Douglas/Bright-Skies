f7334667d6a4182b580e9499690298f55bacf2cf
Jonah Douglas <douglaj8@wwu.edu>
2020-03-13 11:09:22 -0700

    results goes to correct screen, thinking of what to put in results

--- app/src/main/java/com/example/bright_skies/fragments/ResultsFragment.java
+++ app/src/main/java/com/example/bright_skies/fragments/ResultsFragment.java
@@ -13,8 +13,21 @@ import com.example.bright_skies.R;
 public class ResultsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_about_us, container, false);
+        View root = inflater.inflate(R.layout.fragment_results, container, false);
 
         return root;
     }
+
+    public String solarRecommendation(int dni) {
+        String result;
+
+        if (dni < 1000) {
+            result = "Your location does not have the required solar irradiance to allow for solar panels to be viable";
+        } else if (dni < 1500) {
+            result = "Average";
+        } else {
+            result = "Good";
+        }
+    }
+
 }
